233c3a27315e32ec9272a97cb873337ed34e3acb
Curtis H <sitrucrettah1@gmail.com>
2020-11-15 13:06:14 -0800

    Add global variables for storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,26 @@
+//global variables
+var pwSize = 8;
+var specialValue = false;
+var numericValue = false;
+var upperCase = false;
+var lowerCase = false;
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 //Ask what they would like in their password
 function userInput() {
-  var pwSize = prompt("How long is your password? Please choose between 8 and 128.", 8);
+  pwSize = prompt("How long is your password? Please choose between 8 and 128.", 8);
   if (pwSize < 7 || pwSize > 129) {
     console.log("Your password length is out of bounds! Try again.");
     return;
   }
-  var lowerCase = confirm("Lower case characters included?");
-  var upperCase = confirm("Upper case characters included?");
-  var numericValue = confirm("Numbers included?");
-  var specialValue = confirm("Special characters included?");
+  lowerCase = confirm("Lower case characters included?");
+  upperCase = confirm("Upper case characters included?");
+  numericValue = confirm("Numbers included?");
+  specialValue = confirm("Special characters included?");
+
+  console.log(pwSize + " " + specialValue + " " + numericValue + " " + upperCase + " " + lowerCase)
 }
 
 // Generate Password
