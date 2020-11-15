9ee9e20e92815de99b397eb2e06ac71fc4348d29
Curtis H <sitrucrettah1@gmail.com>
2020-11-15 12:49:48 -0800

    Add function userInput() for testing prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Ask what they would like in their password
+function userInput() {
+  var pwSize = prompt("How long is your password? Please choose between 8 and 128.", 8);
+  if (pwSize > 7 && pwSize < 129) {
+    console.log("Your password length is: " + pwSize + "!");
+  }
+
+}
+
+// Generate Password
+function generatePassword() {
+  userInput();
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
